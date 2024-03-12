81d5a41ff1ea991f8dc192b10a728c7aeb9d5150
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2024-03-12 14:02:50 +0300

    Add files via upload

---
+++ muh10a.sql
@@ -0,0 +1,16 @@
+INSERT INTO butce 
+VALUES(NULL,'Ocak Maaş',1,20000,'2024-01-15');
+
+
+INSERT INTO butce
+VALUES(NULL,'Kırtasiye',2,150,'2024-01-20');
+
+
+INSERT INTO butce 
+VALUES(NULL,'Kira',3,5000,'2024-01-20');
+
+
+INSERT INTO butce
+VALUES(NULL,'Ocak Su',4,150,'2024-02-05');
+
+
